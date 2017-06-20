92a191e756874aeefffbc3fc92f82e280b86bb41
Konstantin Starostin <konstantin.starostin@jetbrains.com>
2017-06-20 20:24:41 +0300

    codereview commit

--- Project/src/src/Timer.java
+++ Project/src/src/Timer.java
@@ -1,18 +1,19 @@
 package src;
 
-import javax.swing.*;
 
 public class Timer {
+// основной класс для тестирования структуры
     public static void main(String[] args) {
+// создаем объекты, которые уже есть
+        Journey j = new Journey();
+        Meeting m = new Meeting();
+        Events e = new Events();
 
-Journey j = new Journey();
-Meeting m = new Meeting();
-Events e = new Events();
-
-Events[]x = new Events[3];
-x [0] = j;
-x [1] = m;
-x [2] = e;
+        Events[] x = new Events[3];
+        x[0] = j;
+        x[1] = m;
+        x[2] = e;
         for (Events aX : x) aX.action();
+// проверяем что могут
     }
 }
